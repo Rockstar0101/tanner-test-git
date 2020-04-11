858bdea169abacc1ebfd2a3cfd52d569dc55c8a9
Rockstar 0101 <govindsaini0101@gmail.com>
2020-04-11 15:09:34 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+
+function drop(){
+    document.getElementById('toggle').classList.toggle('toggle2');
+    document.getElementById('drop2').classList.toggle('cross');
+    document.getElementById('drop').classList.toggle('cross2');
+}
